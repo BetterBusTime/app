8e6df5c81e99542fe571a78ac8543daf778ecb2c
fahali <fahim.ali.204@gmail.com>
2021-02-26 20:24:15 -0500

    header style

--- src/App.js
+++ src/App.js
@@ -9,26 +9,33 @@ import BusStop from "./components/BusStop";
 function App() {
     return (
         <div className='App'>
-            <Switch>
-                <Route
-                    path='/boros/:id'
-                    render={({ match }) => <Boro boroKey={match.params.id} />}
-                />
-                <Route path='/boros' render={() => <Boros />} />
-                <Route
-                    path='/routes/:id'
-                    render={({ match }) => (
-                        <BusRoute routeId={match.params.id} />
-                    )}
-                />
-                <Route
-                    path='/stops/:id'
-                    render={({ match }) => (
-                        <BusStop stopCode={match.params.id} />
-                    )}
-                />
-                <Route path='/' render={() => <Redirect to='/boros' />} />
-            </Switch>
+            <header>
+                <h1 className='title-banner'>Better Bus Time</h1>
+            </header>
+            <main>
+                <Switch>
+                    <Route
+                        path='/boros/:id'
+                        render={({ match }) => (
+                            <Boro boroKey={match.params.id} />
+                        )}
+                    />
+                    <Route path='/boros' render={() => <Boros />} />
+                    <Route
+                        path='/routes/:id'
+                        render={({ match }) => (
+                            <BusRoute routeId={match.params.id} />
+                        )}
+                    />
+                    <Route
+                        path='/stops/:id'
+                        render={({ match }) => (
+                            <BusStop stopCode={match.params.id} />
+                        )}
+                    />
+                    <Route path='/' render={() => <Redirect to='/boros' />} />
+                </Switch>
+            </main>
         </div>
     );
 }
