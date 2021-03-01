4ea179e6af41d0dbd6150695930c85925e9a4a8a
fahali <fahim.ali.204@gmail.com>
2021-03-01 18:50:09 -0500

    pin button

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,19 +1,27 @@
 import { useHistory } from "react-router-dom";
+import pin from "../paper-push-pin.svg";
 
 export default function Routes({ routes }) {
     const history = useHistory();
 
     return (
-        <div className='route-buttons buttons-container'>
+        <div className='route-buttons-container buttons-container'>
             {routes.map(route => (
-                <button
-                    type='button'
-                    key={route.id}
-                    className='route-button control-button'
-                    style={{ backgroundColor: `#${route.color}` }}
-                    onClick={() => history.push(`/routes/${route.id}`)}>
-                    {route.shortName} - {route.longName}
-                </button>
+                <div key={route.id} className='route-buttons'>
+                    <button
+                        type='button'
+                        className='route-button control-button'
+                        style={{ backgroundColor: `#${route.color}` }}
+                        onClick={() => history.push(`/routes/${route.id}`)}>
+                        {route.shortName} - {route.longName}
+                    </button>
+                    <img
+                        src={pin}
+                        alt='pin'
+                        className='pin-button'
+                        style={{ backgroundColor: `#${route.color}` }}
+                    />
+                </div>
             ))}
         </div>
     );
